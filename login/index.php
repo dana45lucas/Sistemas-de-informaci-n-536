5cc97612bedde33f884c5cd20312a5a0bc877df9
adalid028 <45442553+adalid028@users.noreply.github.com>
2018-11-30 17:16:31 -0600

    cambio de titulo

--- login/index.php
+++ login/index.php
@@ -9,7 +9,7 @@ if(isset($_SESSION['session']))
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<title>INFTEC</title>
+	<title>sistemas de informacion</title>
 	<meta charset="utf-8">
 	<link type="text/css" href="./css/style.css" rel="stylesheet" />
 </head>
