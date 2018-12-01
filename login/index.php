c942f6584596b3d5ce330128c1267710008e9358
irving13joas <joas_cobao@hotmail.com>
2018-11-30 22:54:21 -0600

    cambio de codigo

--- login/index.php
+++ login/index.php
@@ -35,7 +35,7 @@ if(isset($_SESSION['session']))
 					<p><label >colaborador 1 :</label></p>
 						<input name="usuario" type="text" id="usuario" placeholder="Ingresa Usuario" autofocus="" required=""></p>
 					
-					<p><label>codigo :</label></p>
+					<p><label>contraseña2 :</label></p>
 						<input name="contrasenia" type="password" id="contrasenia" placeholder="Ingresa Password" required=""></p>
 					
 					<p id="bot"><input type="submit" id="submit" name="submit" value="Ingresar" class="boton"></p>
