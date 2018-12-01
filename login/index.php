0361a882cc81f3ff97ae92d037ae133252f786f4
irving13joas <joas_cobao@hotmail.com>
2018-11-30 19:43:13 -0600

    cambio de registro

--- login/index.php
+++ login/index.php
@@ -17,7 +17,7 @@ if(isset($_SESSION['session']))
 <body>
 	
 	<div id="registrar">
-          	<a href="./php/registro.php"</a>Registrar</a>
+          	<a href="./php/registro.php"</a>iniciarregistro</a>
     </div> <!-- fin opcion-->
     
 	<div id="envoltura">
