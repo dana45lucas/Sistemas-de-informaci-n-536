80768b9fc7ab5103165b7f75c71ea33114b64672
irving13joas <joas_cobao@hotmail.com>
2018-12-03 21:33:12 -0600

    nuevocliente

--- login/index.php
+++ login/index.php
@@ -17,7 +17,7 @@ if(isset($_SESSION['session']))
 <body>
 	
 	<div id="registrar">
-          	<a href="./php/registro.php"</a>ingresatunombre</a>
+          	<a href="./php/registro.php"</a>nuevocliente</a>
     </div> <!-- fin opcion-->
     
 	<div id="envoltura">
